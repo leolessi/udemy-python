35d940f5457c62a0edc637deffbcc216e85ff634
leolessi <leonardolessi@gmail.com>
2025-07-25 20:02:40 -0300

    add aula16

---
+++ Section 03/aula16_condicionais.py
@@ -0,0 +1,23 @@
+# entrada = input("Voce deseja entrar ou sair? ")
+
+# if entrada == "entrar":
+#     print("Voce entrou no sistema")
+# elif entrada == "sair":
+#     print("Voce saiu do sistema")
+# else:
+#     print("Voce nao digitou uma opcao valida")
+
+condicao = False
+numero = 10
+
+if condicao:
+    print("Este é o código do if")
+else:
+    print("Este é o código do else")
+
+if numero == 10:
+    print("Número vale 10 (int)")
+elif numero == "10":
+    print("Número vale 10 (str)")
+else:
+    print("Nenhum número 10 encontrado.")
