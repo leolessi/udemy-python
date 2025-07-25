e069221000407b2266670bdbd7579f4a732fab8f
leolessi <leonardolessi@gmail.com>
2025-07-25 18:50:51 -0300

    add aula12

--- Section 3/aula12_imc.py
+++ Section 3/aula12_imc.py
@@ -3,6 +3,10 @@ altura = 1.70
 peso = 120
 imc = peso / (altura * 2)
 
-print(
-    f"{nome} tem {altura:.2f} de altura, \npesa {peso} quilos e seu IMC é \n{imc:.4f}"
-)
+# print(
+#     f"{nome} tem {altura:.2f} de altura, \npesa {peso} quilos e seu IMC é \n{imc:.4f}"
+# )
+
+print("{} tem {:.2f} de altura".format(nome, altura))
+print("Pesa {} quilos".format(peso))
+print("Tem IMC = {:.4f}".format(imc))
