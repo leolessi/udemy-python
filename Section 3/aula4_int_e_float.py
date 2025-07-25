275f2f33257408e2e4d87bc698f94132e7ba07b5
leolessi <leonardolessi@gmail.com>
2025-07-25 13:20:01 -0300

    add aula4

---
+++ Section 3/aula4_int_e_float.py
@@ -0,0 +1,12 @@
+# int é qualquer número inteiro, positivo ou negativo
+print(11)
+print(-11)
+print(0)
+
+# float representa qualquer número, positivo ou negativo, com ponto flutuante
+# float sem sinal é considerado positivo
+# SEMPRE deve-se usar ponto para separar as casas decimais
+print(1.1, -1.11, sep=" | ")
+
+# para descobrir o tipo
+print(type(1.1), type(-1.11), type(0))
