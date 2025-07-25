0a70db41e3ac2b31adb8e36df3b6390aa99ae8b5
leolessi <leonardolessi@gmail.com>
2025-07-25 13:26:09 -0300

    add aula5

---
+++ Section 3/aula5_boolean.py
@@ -0,0 +1,6 @@
+# ao questionar algo em programação, só existem duas repsostas possíveis:
+# sim  / não | True / False | 0 / 1
+print(10 == 10)
+print(10 == 11)
+print(type(10 == 10))
+print(type(False))
