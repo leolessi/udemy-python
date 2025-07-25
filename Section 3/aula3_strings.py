a5118801ad0fe12a6d62fa1cdef8448aa6ac35de
leolessi <leonardolessi@gmail.com>
2025-07-25 13:02:03 -0300

    add aula3

---
+++ Section 3/aula3_strings.py
@@ -0,0 +1,9 @@
+# String sempre está contida dentro de aspas (duplas ou simples)
+print("123")
+
+# print(1234) imprime um inteiro
+
+# Para mostrar aspas dentro de aspas, utilizar aspas duplas + simpes
+# ou aspas simples + duplas
+print('Leonardo "Theta" Lessi')
+print("Leonardo 'Theta' Lessi")
