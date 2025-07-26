158ed20d484f33eaaae3591453cc389498cb3aac
leolessi <leonardolessi@gmail.com>
2025-07-25 22:42:06 -0300

    add aula26

---
+++ Section 03/aula26_formatacao_strings_com_fstrings.py
@@ -0,0 +1,8 @@
+variavel = "ABC"
+numero = 1500
+
+print(f"{variavel}.")
+print(f"{variavel:->10}.")
+print(f"{variavel:-<10}.")
+print(f"{variavel: ^10}.")
+print(f"O hexadecimal de {numero} vale {numero:08X}")
