86199ed76ba4d38e9951c0c0e189db17a9855ae9
leolessi <leonardolessi@gmail.com>
2025-07-29 00:39:59 -0300

    add aula31

---
+++ Section 03/aula31_is_isnot_none.py
@@ -0,0 +1,13 @@
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print("Faça algo")
+else:
+    print("Não faça nada")
+
+if passou_no_if is None:
+    print("Não passou no if")
+else:
+    print("Passou no if")
