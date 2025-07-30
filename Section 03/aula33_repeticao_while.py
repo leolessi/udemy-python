30e77098e3c40cbca08be2175c8a0a882c1b2713
leolessi <leonardolessi@gmail.com>
2025-07-29 23:46:02 -0300

    att aula33

--- Section 03/aula33_repeticao_while.py
+++ Section 03/aula33_repeticao_while.py
@@ -1,12 +1,44 @@
-condicao = False
+# condicao = False
 
-while not condicao:
-    senha = input("Qual a senha para sair? ")
+# while not condicao:
+#     senha = input("Qual a senha para sair? ")
 
-    if senha != "33":
-        print(f"Senha invalida. Tente novamente.")
-    else:
-        condicao
+#     if senha != "33":
+#         print(f"Senha invalida. Tente novamente.")
+#     else:
+#         condicao
+#         break
+
+# print("Senha correta. Fim do programa.")
+#
+#
+#
+#
+#
+# contador = 0
+
+# while contador < 10:
+#     print(f"Contagem: foi acessado {contador + 1} vezes.")
+#     contador += 1
+
+# print("Fim do programa!")
+
+
+# o continue faz voltar para a cabeça do while mais proximo
+contador = -1
+
+while contador <= 100:
+    contador += 1
+
+    if contador == 6:
+        print("Nao vou mostrar o 6")
+        continue
+
+    if 27 >= contador >= 10:
+        print("Nao vou mostrar o ", contador)
+        continue
+
+    print(contador)
+
+    if contador == 33:
         break
-
-print("Senha correta. Fim do programa.")
